1841d6465ef70279caf360634dfbe70397d3ecdf
Emir Planic <plana799@gmail.com>
2019-12-29 21:31:02 +0100

    Added BasicEnemy

---
+++ src/Main/BasicEnemy.java
@@ -0,0 +1,35 @@
+package Main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BasicEnemy extends GameObject {
+
+	public BasicEnemy(int x, int y, ID id) {
+		super(x, y, id);
+		
+	//	velX=5;
+		velY=5;
+		
+		
+	}
+
+	@Override
+	public void tick() {
+		x+=velX;
+		y+=velY;
+			
+		if(y<0 || y>= Game.HEIGHT - 32) velY *= -1;
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.red);
+		g.fillRect(x, y, 16, 16);
+	}
+
+	
+	
+}
