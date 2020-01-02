43a2292be73a690ce1c349649ab856c5770aa42a
Emir Planic <plana799@gmail.com>
2020-01-02 21:41:34 +0100

    implemented velX

--- src/Main/BasicEnemy.java
+++ src/Main/BasicEnemy.java
@@ -8,7 +8,7 @@ public class BasicEnemy extends GameObject {
 	public BasicEnemy(int x, int y, ID id) {
 		super(x, y, id);
 		
-	//	velX=5;
+		velX=5;
 		velY=5;
 		
 		
@@ -20,6 +20,7 @@ public class BasicEnemy extends GameObject {
 		y+=velY;
 			
 		if(y<0 || y>= Game.HEIGHT - 32) velY *= -1;
+		if(x<0 || x>= Game.WIDTH - 16) velX *= -1;
 		
 	}
 
